7594ced2a64f528b35e4e5d7e3f329d6f697cbd1
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-05-19 21:50:14 -0400

    refactored for easier debugging

--- data_access.py
+++ data_access.py
@@ -8,31 +8,26 @@ __author__ = 'andriod'
 
 
 
-def get_dummy(env_name):
-    return Dingus("{env_name}_env".format(env_name=env_name))
+def get_dummy(coll_name):
+    return Dingus("{coll_name}_env".format(coll_name=coll_name))
 
-def get_file(env_name):
-    return FileHolder(env_name)
+def get_file(coll_name):
+    return FileHolder(coll_name)
 
-def get_live(env_name):
-    env = FileHolder(env_name)
+def get_live(coll_name):
+    env = FileHolder(coll_name)
     env.fx.usd_eur=QuandleAsset("GOOG/NYSE_ERO")
+    env.fx.usd_jpy=QuandleAsset("QUANDL/USDJPY")
     return env
-
-class StoredEnv(Env):
-    pass
-
-
-def get_stored(env_name):
-    return StoredEnv(env_name)
-
 
 if __name__ == "__main__":
     print(repr(get_dummy("test")))
 
-    print(repr(get_file("market1")))
-    print(repr(get_file("market1").news))
-    print(repr(get_file("market1").news['IBM']))
-    print(repr(get_file("market1").news['IBM']['5-14-2010']))
+    fileCollection = get_file("market1")
+    print(repr(fileCollection))
+    print(repr(fileCollection.news))
+    print(repr(fileCollection.news['IBM']))
+    print(repr(fileCollection.news['IBM']['5-14-2010']))
 
-    print(repr(get_live("market1").fx.usd_eur.Volume['2010-05-14']))
+    liveCollection = get_live("market2")
+    print(repr(liveCollection.fx.usd_eur.Volume['2010-05-14']))
