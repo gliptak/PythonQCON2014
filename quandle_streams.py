04d7ed0af83f8ff7a25aadae8e9f813ef3b3f1d4
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-05-26 16:09:57 -0400

    added sample data added model and ccyConfig added code to pull all Quandl fx rates and store as CSV

--- quandle_streams.py
+++ quandle_streams.py
@@ -1,5 +1,6 @@
 __author__ = 'andriod'
 
+from itertools import chain
 import Quandl
 
 
@@ -19,8 +20,32 @@ class QuandleAsset(object):
         return getattr(self.value, item)
 
 
+def name_to_code(query='USD', authtoken=None):
+    currPage = True
+    quandlAll = []
+    pageNo = 0
+    while currPage:
+        currPage = Quandl.search(query, source="QUANDL", prints=False, page=pageNo, authtoken=authtoken)
+        quandlAll.append(currPage)
+        pageNo += 1
+    return {"%s_%s" % (curve['code'][7:10].lower(), curve['code'][10:].lower()): curve['code'] for curve in
+            chain(*quandlAll)}
+
+
+def dl_quandl(query="USD", authtoken=None):
+    count = 0
+    for name, code in name_to_code(query=query, authtoken=authtoken).items():
+        curve = Quandl.get(code, authtoken=authtoken)
+        curve.to_csv(name + ".csv")
+        count += 1
+    print("Got %d curves" % count)
+
+
 if __name__ == "__main__":
     test = QuandleAsset("GOOG/NYSE_ERO")
     print(test)
     print(test.value)
     print(test.Close['2009-05-14'])
+
+    dl_quandl("USD")
+    #dl_quandl("EUR")
