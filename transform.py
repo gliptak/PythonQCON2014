a836f65309858fc98e0d0e9800b3f9495e5b6497
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-05-26 16:50:32 -0400

    added docstring

--- transform.py
+++ transform.py
@@ -3,4 +3,10 @@ __author__ = 'andriod'
 import pandas as pd
 
 def make_ccy_matrix(market, model):
+    """
+
+    :param market: - a market data collection
+    :param model: - a model data collection
+    :return: a Dataframe with rows of dates and columns of fx rates per ccy
+    """
     return pd.DataFrame([curve.Rate for curve in [market.fx[ccy+'_usd'] for ccy in model.ccyConfig]]).T
