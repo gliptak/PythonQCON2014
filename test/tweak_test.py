ccb28ace9ec7e005f471d02560370ecc078a73c9
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-06-01 20:44:28 -0400

    Reformatted code

--- test/tweak_test.py
+++ test/tweak_test.py
@@ -1,19 +1,20 @@
 from operator import mul
+import unittest
+
 from data_access import get_file
 from stress import point_tweak
 
-__author__ = 'andriod'
 
-import unittest
+__author__ = 'andriod'
 
 
 class PointTestCase(unittest.TestCase):
     def test_point_tweak(self):
         market = get_file("market1")
         preTweak = market.fx.usd_eur.Rate['2010-5-14']
-        tweakedMarket = point_tweak(market,'fx.usd_eur.Rate','2010-5-14',mul,1.1)
+        tweakedMarket = point_tweak(market, 'fx.usd_eur.Rate', '2010-5-14', mul, 1.1)
         postTweak = tweakedMarket.fx.usd_eur.Rate['2010-5-14']
-        self.assertAlmostEqual(preTweak*1.1,postTweak)
+        self.assertAlmostEqual(preTweak * 1.1, postTweak)
 
 
 if __name__ == '__main__':
